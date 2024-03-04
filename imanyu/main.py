4ee4f427b6e3376fed54bd6964662e0520d1d8c9
kenta-afk <kentarou9.60@icloud.com>
2024-03-04 13:10:35 +0900

    learn about query param

--- imanyu/main.py
+++ imanyu/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from typing import Optional
 
 app = FastAPI()
 
@@ -7,11 +7,19 @@ app = FastAPI()
 async def index():
     return {"message": "Hello World"}
 
-@app.get("/countries/Japan")
-async def Japan():
-    return {"message": "This is Japan"}
-
 @app.get("/countries/{country_name}")
-async def country(country_name: str):
-    return {"country_name": country_name}
+async def country(country_name: str = "japan", city_name: str = "tokyo"):
+    return {
+        "country_name": country_name,
+        "city_name": city_name
+    }
+
+#http://127.0.0.1:8000/countries/america?city_name=new_york
+
+@app.get("/countries/")
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+    return {
+        "country_name": country_name,
+        "country_no": country_no
+    }
 
